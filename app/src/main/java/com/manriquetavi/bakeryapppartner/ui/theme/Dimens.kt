9c6162b52d84a10be4ab9590446e6027dd947227
Tavi <tavidanner96@gmail.com>
2022-08-05 01:35:51 -0500

    set common dimens

---
+++ app/src/main/java/com/manriquetavi/bakeryapppartner/ui/theme/Dimens.kt
@@ -0,0 +1,11 @@
+package com.manriquetavi.bakeryapppartner.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val EXTRA_LARGE_PADDING = 40.dp
+val LARGE_PADDING = 20.dp
+val MEDIUM_PADDING = 16.dp
+val SMALL_PADDING = 10.dp
+val EXTRA_SMALL_PADDING = 6.dp
+
+val PRODUCT_ITEM_HEIGHT = 164.dp
